a1d923abbcf0064bca36e4f1f4f050b63521c36e
Wendelin Gerein <info@webdelin.de>
2021-10-07 13:18:02 +0200

    dotenv success all DB

--- app/server.js
+++ app/server.js
@@ -1,11 +1,8 @@
 import app from './app';
 import {PORT} from './config';
-import ENV, { IS_DEV } from './utils/env';
 
 const server = app.listen(PORT, (error) => {
   if (error) console.log(error);
-  console.log(ENV);
-  console.log(IS_DEV);
   console.log(`Server running on PORT: ${PORT}`);
 });
 
